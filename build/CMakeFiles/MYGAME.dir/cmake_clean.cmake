9a56d6399f2046db3624b9927afe26d7dbbf221a
JDiegoS <46642689+JDiegoS@users.noreply.github.com>
2022-11-22 03:07:19 -0600

    final

---
+++ build/CMakeFiles/MYGAME.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MYGAME.dir/src/Game/game.cpp.o"
+  "CMakeFiles/MYGAME.dir/src/Scene/Scene.cpp.o"
+  "CMakeFiles/MYGAME.dir/src/main.cpp.o"
+  "MYGAME"
+  "MYGAME.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MYGAME.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
